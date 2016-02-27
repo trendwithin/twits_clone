315437e43551f3a207b5195b4c8f4db8600b1c9c
Michael Becco <e4e6d4d5@gmail.com>
2016-02-27 13:38:46 -0800

    Modify Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Users
+User.create(email: 'test_admin@example.com', password: 'password', password_confirmation: 'password', role: 'admin')
+User.create(email: 'me@you.com', password: 'password', password_confirmation: 'password')
+
 
 # Blogs
 
-Blog.create(title: 'First Title', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum velit numquam harum voluptates amet. Veniam voluptate magni, iste et perferendis officiis architecto adipisci corrupti autem laborum quidem magnam reprehenderit quibusdam!')
-Blog.create(title: 'Second Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam repellendus eveniet cum architecto explicabo unde ex deleniti culpa perferendis quidem aspernatur mollitia, numquam, iusto recusandae velit sed eaque omnis a!')
-Blog.create(title: 'Third Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore numquam quasi impedit accusamus quod soluta animi, nisi eveniet voluptas nam temporibus sint suscipit inventore est eaque expedita incidunt nostrum id!')
-Blog.create(title: 'Fourth Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga itaque molestias, ullam asperiores neque optio. Hic deserunt at cumque ipsum saepe, dolores similique quod in obcaecati mollitia accusamus culpa molestiae?')
-Blog.create(title: 'Fifth Blog' , body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores veniam, iste eligendi nam ab, ipsum in et illum assumenda praesentium, optio placeat! Nobis repellendus quibusdam modi, quo iure perspiciatis suscipit!')
-Blog.create(title: 'Sixth Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsum saepe labore, ipsa harum cum minima modi magnam repudiandae sit neque unde itaque mollitia quia enim. Aliquid omnis repellendus, aspernatur?')
+Blog.create(title: 'First Title', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum velit numquam harum voluptates amet. Veniam voluptate magni, iste et perferendis officiis architecto adipisci corrupti autem laborum quidem magnam reprehenderit quibusdam!', user_id: User.first.id)
+Blog.create(title: 'Second Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam repellendus eveniet cum architecto explicabo unde ex deleniti culpa perferendis quidem aspernatur mollitia, numquam, iusto recusandae velit sed eaque omnis a!', user_id: User.first.id)
+Blog.create(title: 'Third Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore numquam quasi impedit accusamus quod soluta animi, nisi eveniet voluptas nam temporibus sint suscipit inventore est eaque expedita incidunt nostrum id!', user_id: User.first.id)
+Blog.create(title: 'Fourth Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga itaque molestias, ullam asperiores neque optio. Hic deserunt at cumque ipsum saepe, dolores similique quod in obcaecati mollitia accusamus culpa molestiae?', user_id: User.first.id)
+Blog.create(title: 'Fifth Blog' , body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores veniam, iste eligendi nam ab, ipsum in et illum assumenda praesentium, optio placeat! Nobis repellendus quibusdam modi, quo iure perspiciatis suscipit!', user_id: User.first.id)
+Blog.create(title: 'Sixth Blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsum saepe labore, ipsa harum cum minima modi magnam repudiandae sit neque unde itaque mollitia quia enim. Aliquid omnis repellendus, aspernatur?', user_id: User.first.id)
+
+
