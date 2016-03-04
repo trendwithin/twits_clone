24ea985ce1ea06e37663d1e44a03e62b10774a19
Michael Becco <e4e6d4d5@gmail.com>
2016-03-04 14:37:54 -0800

    Test Order of Comments

---
+++ test/features/comments/ordering_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+feature "Comments::Ordering" do
+  scenario "Comments are ordered in desc order" do
+    logged_in_as users(:vic)
+    visit blog_path blogs(:one)
+    fill_in 'comment[body]', with: 'Second to first'
+    click_button 'Add Comment'
+    fill_in 'comment[body]', with: 'This should be the first comment returned'
+    click_button 'Add Comment'
+
+    comments = blogs(:one).comments
+    byebug
+    assert_equal 'This should be the first comment returned', Blog.first.comments.desc.first.body
+  end
+end
